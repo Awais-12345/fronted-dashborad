8234a4f11086627994ba427b2611c71bcd409a70
Awais-12345 <awaisraza2131@gmail.com>
2025-08-30 14:57:24 -0700

    ddddd

--- src/components/utils/interceptor.js
+++ src/components/utils/interceptor.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://backend-code-woad-delta.vercel.app/',
+  baseURL: 'http://localhost:3000/',
   headers: { 'Content-Type': 'application/json' },
   timeout:10000,
 });
